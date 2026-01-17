06de90d17a5597f8ae3d1419ad4a13a52facc965
Alex31-del <alexandernavarroseminario@gmail.com>
2026-01-17 18:27:18 -0500

    Modify mostrarResultados and main method parameters
    Se actualizó el método mostrarResultados para incluir las horas de inicio y finalización.

--- Aplicacion.java
+++ Aplicacion.java
@@ -6,18 +6,24 @@ public class Aplicacion {
                   Aplicacion ap;
                   ap = new Aplicacion();
                   ArrayList<contMulti> playList;
-
+                  double inicioPlay, finPlay,tiempoTotal;
                  
                   playList = ap.agregarContenido();
                   inicioPlay = System.nanoTime();
 
                   finPlay = System.nanoTime();
                   tiempoTotal = finPlay - inicioPlay;
-                  ap.mostrarResultados(playLis, tiempoTotal);
+                  ap.mostrarResultados(playList, inicioPlay, finPlay,tiempoTotal);
          }
          
      public ArrayList<contMulti> agregarContenido(){
         ArrayList<contMulti> arr;
         return arrs; 
     }    
+     public mostrarResultados(ArrayList<contMulti> playList, double inicioPlay, double finPlay,double tiempoTotal){
+
+              System.out.println("tiempo inicial de la cancion: "+ inicioPlay);
+              System.out.println("tiempo final de la cancion: "+ finPlay);
+              System.out.println("El tiempo total es: "+ tiempoTotal);
+     }         
 }
