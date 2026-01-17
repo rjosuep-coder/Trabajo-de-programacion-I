9ba68a80a1766b64231d8b888499f7c20d241ced
Crisque22 <rumichevidal@gmail.com>
2026-01-17 17:24:32 -0500

    Add files via upload

---
+++ Aplicacion.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Aplicacion {
+         public static void main( String arg[]){
+                Aplicacion ap;
+                ap = new Aplicacion();       
+                
+               
+         }
+         
+}
