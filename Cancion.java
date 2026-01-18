0948798ded9c5a8e3a7fe1739e80fb05659074fd
Alex31-del <alexandernavarroseminario@gmail.com>
2026-01-17 19:40:08 -0500

    Update Cancion.java
    Cree un nuevo metodo pedir Datos

--- Cancion.java
+++ Cancion.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Cancion extends ContMulti {
     private String artista;
 
@@ -13,7 +14,29 @@ public class Cancion extends ContMulti {
     public void mostrarDetalles() {
         System.out.println("[CANCION] " + getTitulo() + " | Artista: " + artista + " | " + getDuracion() + "s");
     }
+    public  Cancion pedirDatos() {
+        String titulo,artista;
+        double duracion;
+        Scanner tex;
+        Cancion c;
+        tex= new Scanner(System.in);
+        
+        System.out.print("Introduce el título: ");
+        String titulo = sc.nextLine();
+
+        System.out.print("Introduce el artista: ");
+        String artista = sc.nextLine();
+
+        System.out.print("Introduce la duración (segundos): ");
+         duracion = sc.nextDouble();
+    
+        //Limpiar el buffer después de leer números
+        sc.nextLine(); 
+         c = new Cancion(titulo, duracion, artista);
+        return c;
+    }
 }
  
     
+
 
