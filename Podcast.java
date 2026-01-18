1a23ec6e3f1423864eb3a5956a194c70c6dd0309
Crisque22 <rumichevidal@gmail.com>
2026-01-17 20:03:57 -0500

    Update Podcast.java

--- Podcast.java
+++ Podcast.java
@@ -1,8 +1,8 @@
 public class Podcast extends ContMulti{
     int episodio;
 
-    public Podcast(int episodio, String titulo, int duracionSegundos) {
-        super(titulo, duracionSegundos);
+    public Podcast(int episodio, String titulo, int duracion) {
+        super(titulo, duracion);
         this.episodio = episodio;
     }
 
@@ -13,5 +13,36 @@ public class Podcast extends ContMulti{
     public void setEpisodio(int episodio) {
         this.episodio = episodio;
     }
+
+    @Override
+    public void pedirDatos() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Titulo del podcast: ");
+        setTitulo(sc.nextLine());
+
+        System.out.print("Numero de episodio: ");
+        numeroEpisodio = sc.nextInt();
+
+        System.out.print("Duracion (segundos): ");
+        setDuracion(sc.nextInt());
+    }
+
+    @Override
+    public void reproducir() {
+        System.out.println("Reproduciendo podcast: "
+                + titulo
+                + " | Episodio "
+                + numeroEpisodio);
+    }
+
+    @Override
+    public void mostrar() {
+        System.out.println("[PODCAST] "
+                + titulo
+                + " | Ep. " + episodio
+                + " | " + duracion + "s");
+    }
         
 }
+
